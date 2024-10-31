fa16a70e61479e1e7640049f54f892891b0a8d55
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-10-30 22:39:41 -0400

    Update Lab6P.js

--- Lab6P.js
+++ Lab6P.js
@@ -1,3 +1,6 @@
+
+
+// Naviagation part Where you are able to move to different links when clicked 
 document.getElementById('navItem1').addEventListener('click', function() {
     window.location.href = 'Lab5_ITM780.html' })
 
@@ -7,7 +10,7 @@ document.getElementById('navItem2').addEventListener('click', function() {
 document.getElementById('navItem3').addEventListener('click', function() {
     window.location.href = 'Contact.html' })
 
-
+    // Drop Down menu on the Naviagation bar 
 function showDropdown() {
     document.getElementById("dropdown").classList.add("show") }
 

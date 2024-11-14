2d2b86f95c5956d336f827150623a66d22713608
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-11-13 23:36:22 -0500

    Create Lab8.js

---
+++ Lab8.js
@@ -0,0 +1,21 @@
+
+function enlargeText() {document.body.style.fontSize = "25px";}
+
+
+ function setColorScheme(type) {
+    if (type === 'normal') {
+      document.body.style.color = "#000000 "; 
+      document.body.style.backgroundColor = "#0000"; 
+    } else if (type === 'deuteranopia') {
+      document.body.style.color = "#0F4C81";
+      document.body.style.backgroundColor = "#E4F5F9";
+    } else if (type === 'protanopia') {
+      document.body.style.color = "#2F4F4F ";
+      document.body.style.backgroundColor = "#F0F8FF ";
+    } else if (type === 'tritanopia') {
+      document.body.style.color = "#2E2E2E  "; 
+      document.body.style.backgroundColor = "#FAFAFA  ";}}
+
+
+function showAltText(imageId, altTextId) {
+      const altTextElement = document.getElementById(altTextId); altTextElement.style.display = altTextElement.style.display === "none" ? "block" : "none";}

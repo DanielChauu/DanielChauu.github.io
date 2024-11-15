065214a6b512a8fb766b46121018f06c48f7a20c
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-11-14 19:52:37 -0500

    Update Lab5.js

--- Lab5.js
+++ Lab5.js
@@ -1,12 +1,15 @@
 document.getElementById("nameButton").addEventListener("click", function() {
     const userName = prompt("Please enter your name");
     document.getElementById("nameDisplay").textContent = `Hello, ${userName}! Welcome to the site.`})
-
 document.getElementById('navItem1').addEventListener('click', function() {
     window.location.href = 'Index.html' })
 
 document.getElementById('navItem2').addEventListener('click', function() {
+    window.location.href = 'Lab5_ITM780.html' })
+
+document.getElementById('navItem3').addEventListener('click', function() {
     window.location.href = 'Contact.html' })
+
 
 
     document.getElementById('photo').addEventListener('click', function() {

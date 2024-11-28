3c24cccd4ad57c39a8632e5bfe162b4f72d10c33
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-11-28 14:24:00 -0500

    Update Lab6P.js

--- Lab6P.js
+++ Lab6P.js
@@ -2,10 +2,10 @@
 
 // Naviagation part Where you are able to move to different links when clicked 
 document.getElementById('navItem1').addEventListener('click', function() {
-    window.location.href = 'Index.html' })
+    window.location.href = 'HomePage.html' })
 
 document.getElementById('navItem2').addEventListener('click', function() {
-    window.location.href = 'Lab5_ITM780.html' })
+    window.location.href = 'Portfolio.html' })
 
 document.getElementById('navItem3').addEventListener('click', function() {
     window.location.href = 'Contact.html' })

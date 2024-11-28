e174621d5f1cd0dd5315ab4ea23e96a77e143a24
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-11-28 16:50:32 -0500

    Update Lab6P.js

--- Lab6P.js
+++ Lab6P.js
@@ -1,15 +1,21 @@
 
 
-// Naviagation part Where you are able to move to different links when clicked 
+// Navigation part where you are able to move to different links when clicked
 document.getElementById('navItem1').addEventListener('click', function() {
-    window.location.href = 'HomePage.html' })
+    window.location.href = 'HomePage.html';
+});
 
 document.getElementById('navItem2').addEventListener('click', function() {
-    window.location.href = 'Portfolio.html' })
+    window.location.href = 'Portfolio.html';
+});
 
 document.getElementById('navItem3').addEventListener('click', function() {
-    window.location.href = 'Contact.html' })
+    window.location.href = 'WorkExperience.html';
+});
 
+document.getElementById('navItem4').addEventListener('click', function() {
+    window.location.href = 'Contact.html';
+});
     // Drop Down menu on the Naviagation bar 
 function showDropdown() {
     document.getElementById("dropdown").classList.add("show") }

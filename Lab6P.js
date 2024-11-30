d46f9626501a0310c081e212f168825689f7cf2f
DanielChauu <Daniel.Chau@Torontomu.ca>
2024-11-29 19:16:05 -0500

    Update Lab6P.js

--- Lab6P.js
+++ Lab6P.js
@@ -24,5 +24,29 @@ function hideDropdown() {
     document.getElementById("dropdown").classList.remove("show")}
    
 
+///Enlarge text when button is clicked
+function enlargeText() {
+    if (document.body.style.fontSize === "25px") {
+        document.body.style.fontSize = "16px"; 
+    } else {
+        document.body.style.fontSize = "25px"; 
+    }
+}
+
+
+/// Changes the colour of the page when each button is clicked
+ function setColorScheme(type) {
+    if (type === 'normal') {
+      document.body.style.color = "#000000 "; 
+      document.body.style.backgroundColor = "#0000"; 
+    } else if (type === 'deuteranopia') {
+      document.body.style.color = "#0F4C81";
+      document.body.style.backgroundColor = "#E4F5F9";
+    } else if (type === 'protanopia') {
+      document.body.style.color = "#2F4F4F ";
+      document.body.style.backgroundColor = "#F0F8FF ";
+    } else if (type === 'tritanopia') {
+      document.body.style.color = "#2E2E2E  "; 
+      document.body.style.backgroundColor = "#FAFAFA  ";}}
 
 
